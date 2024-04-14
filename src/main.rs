0cb84a9687075a8f4a2f1d43d418ad95741275e3
Vlad Buslov <vlad@buslov.dev>
2024-04-14 12:58:23 +0200

    User color-eyre for error reporting

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,16 @@
-// Uncomment this block to pass the first stage
 use std::net::TcpListener;
+use color_eyre::eyre::Result;
 
-fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
+fn main() -> Result<()> {
+    color_eyre::install()?;
+
     println!("Logs from your program will appear here!");
 
-    let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
+    let listener = TcpListener::bind("127.0.0.1:4221")?;
     for stream in listener.incoming() {
-        match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
-            }
-            Err(e) => {
-                println!("error: {}", e);
-            }
-        }
+        let _stream = stream?;
+        println!("accepted new connection");
     }
+
+    Ok(())
 }
